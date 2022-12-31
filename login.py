7d9f611dde444bc26c10cc386e52ee67805d6021
Andres Orozco <andres@aoro.io>
2022-12-30 17:11:59 -0800

    dontenv login

--- login.py
+++ login.py
@@ -1,5 +1,9 @@
 import os
 from wyze_sdk import Client
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 client = Client().login(os.environ['WYZE_EMAIL'], password=os.environ['WYZE_PASSWORD'])
 print(f"access token: {client['access_token']}")
